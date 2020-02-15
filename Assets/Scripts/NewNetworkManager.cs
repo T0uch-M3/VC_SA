0ac7144080c7bd7f2218703f4358ac2cf553ea08
Unknown <adolf.hazmi@yahoo.com>
2020-02-15 17:39:28 +0100

    Movement control separation over two "player" objects

--- Assets/Scripts/NewNetworkManager.cs
+++ Assets/Scripts/NewNetworkManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
 
+[AddComponentMenu("")]
 public class NewNetworkManager : NetworkManager
 {
     public Transform leftPosition;
@@ -12,23 +13,25 @@ public class NewNetworkManager : NetworkManager
     // Start is called before the first frame update
     //void Start()
     //{
-        
+    //    //print("insideeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+
     //}
 
     //// Update is called once per frame
     //void Update()
     //{
-        
+
     //}
 
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
-        base.OnServerAddPlayer(conn);
         print("insideeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+
+        //base.OnServerAddPlayer(conn);
+        //print("insideeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
         Transform start = numPlayers == 0 ? leftPosition : rightPosition;
         GameObject player = Instantiate(playerPrefab, start.position, start.rotation);
         NetworkServer.AddPlayerForConnection(conn, player);
     }
 
-    
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,35 +7,38 @@ public class PlayerScript : NetworkBehaviour
 {
     public GameObject activateVoice;
     public Rigidbody2D rigidbody2d;
-    public NetworkConnection NtConnection;
-
-    private NetworkManager ntManager;
+    public NewNetworkManager ntManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        ntManager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
-        
+        ntManager = GameObject.Find("NetworkManager").GetComponent<NewNetworkManager>();
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rigidbody2d.velocity = new Vector2(0, Input.GetAxisRaw("Vertical")) *
-                                       (400 * Time.fixedDeltaTime);
+        if (isLocalPlayer)
+        {
+            rigidbody2d.velocity = new Vector2(0, Input.GetAxisRaw("Vertical")) *
+                                   (400 * Time.fixedDeltaTime);
+        }
+        
     }
     public override void OnStartClient()
     {
-        print("entered CLIENT START");
+        //print("entered CLIENT START");
         base.OnStartClient();
         //CmdSpawn();
+        
         transform.SetParent(GameObject.Find("ButtonPanel").transform);
         //transform.position = new Vector2(400, 500);
-        transform.position = ntManager.GetStartPosition().position;
+        //transform.position = ntManager.GetStartPosition().position;
     }
-    
 
-    
+
+
 
     //[Command]
     //void CmdSpawn()
